f6faadda56e4a5c71954ba6882e37605bb2ef9a0
Nafis Rayan <nafisrayan@gmail.com>
2025-06-09 04:14:29 +0600

    Refactor code structure for improved readability and maintainability

--- frontend/src/Components/User/pages/Contact.jsx
+++ frontend/src/Components/User/pages/Contact.jsx
@@ -89,9 +89,17 @@ const ContactUsPage = () => {
           <Typography variant="h6" gutterBottom>
             Map
           </Typography>
-          <Box height={400} mt={2}>
-            {/* <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Baneshwor+(Bharosha%20Hospital)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/population/">Population mapping</a></iframe> */}
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2263.1707428429418!2d85.3351713!3d27.6939869!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1999d574786b%3A0x727aa56d32352da2!2z4KSt4KSw4KWL4KS44KS-IOCkheCkuOCljeCkquCkpOCkvuCksiDgpKrgpY3gpLDgpL4u4KSy4KS_!5e1!3m2!1sne!2snp!4v1694709428278!5m2!1sne!2snp" width="850" height="400"  loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <Box height={400} mt={2}>            {/* <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Baneshwor+(Bharosha%20Hospital)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/population/">Population mapping</a></iframe> */}
+            <iframe 
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.2911306924857!2d-73.9857781848628!3d40.75836227932681!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25855c6480299%3A0x55194ec5a1ae072e!2sTimes%20Square%2C%20New%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sus!4v1694709428278!5m2!1sen!2sus" 
+              width="100%" 
+              height="400" 
+              style={{ border: 0 }}
+              allowFullScreen="" 
+              loading="lazy" 
+              referrerPolicy="no-referrer-when-downgrade"
+              title="Health Plus Hospital Location - New York"
+            ></iframe>
           </Box>
         </Box>
 
